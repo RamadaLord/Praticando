974d56b8aaf9f7f7db364da41ab992c218e597ba
Peterson Geraldo <petersongeraldo94@gmail.com>
2023-08-09 17:25:04 -0300

    att

--- lancheteria/src/services/users/CriarUsuarioSerrvices.ts
+++ lancheteria/src/services/users/CriarUsuarioSerrvices.ts
@@ -1,17 +1,38 @@
+import prismaClient from "../../../prisma";
 
-
-interface CriarUsuario{
-    nome: string,
-    email:string,
-    senha:string
+interface CriarUsuario {
+  nome: string;
+  email: string;
+  senha: string;
 }
 
 class CriarUsuarioServices {
+  async excecute({ nome, email, senha }: CriarUsuario) {
+    if (!nome || !email || !senha) {
+      throw new Error("Existem campos em braco, não sao permitidos");
+    }
+    const emailJaCadastrado = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (emailJaCadastrado) {
+      throw new Error("E-mail ja cadastrado");
+    }
 
-  async excecute({ nome, email, senha }:CriarUsuario) {
-
-
-    console.log(nome)
+    const usuario = await prismaClient.user.create({
+        data:{
+            nome:nome,
+            email:email,
+            senha:senha
+        },
+        select:{
+            id:true,
+            nome:true,
+            email:true
+        }
+    })
+    return {nome:usuario}
   }
 }
 
