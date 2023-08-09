be1a6cac13165112f9a24a65448c4cd7378e6b88
Peterson Geraldo <petersongeraldo94@gmail.com>
2023-08-09 15:39:47 -0300

    Att

---
+++ lancheteria/prisma/migrations/20230809181528_/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `usuarios` (
+    `id` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+    `create_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
