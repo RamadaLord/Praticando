e0b453254e155a91aac074c2590e43ed3569b746
Peterson Geraldo <petersongeraldo94@gmail.com>
2023-08-28 15:32:25 -0300

    ttt

--- lancheteria_front_end/lancheteriafe/src/components/formulario.js
+++ lancheteria_front_end/lancheteriafe/src/components/formulario.js
@@ -1,28 +1,52 @@
 import { IMaskInput } from "react-imask";
 import React, { useEffect, useState } from "react";
 import "../css/formulario.css";
+import apiBack from "../services/api";
 function Formulario() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [nome, setNome] = useState("");
 
+  async function handleFormulario(e){
+    e.preventDefault();
+
+    await apiBack.post("/CriarUsuarios",{
+      nome,
+      email,
+      senha
+    })
+  }
   return (
     <div id="formulario">
-      <h1>Login</h1>
-      <form>
-        <label>Email</label>
+      <h1>Login: </h1>
+      <br/>
+      <form onSubmit={handleFormulario}>
+        <label>Nome De Usuario:</label>
+        <IMaskInput
+          id="input"
+          type="text"
+          placeholder="Seu Nome de usuario aqui"
+          value={nome}
+          onChange={(e)=> setNome(e.target.value)}
+        />
+        <br />
+
+        <label>Email:</label>
         <IMaskInput
           id="input"
           type="text"
           placeholder="Seu Email aqui"
           value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <br />
-        <label>Senha</label>
+        <label>Senha:</label>
         <IMaskInput
           id="input"
           type="password"
           placeholder="******"
           value={senha}
+          onChange={(e)=>setSenha(e.target.value)}
         />
         <button className="btn" type="submit">
           Enviar
