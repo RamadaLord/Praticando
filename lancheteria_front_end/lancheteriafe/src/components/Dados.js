9d9ef9e837654870a6c3d88607db79f8765a4a16
Peterson Geraldo <petersongeraldo94@gmail.com>
2023-08-30 18:41:19 -0300

    att

--- lancheteria_front_end/lancheteriafe/src/components/Dados.js
+++ lancheteria_front_end/lancheteriafe/src/components/Dados.js
@@ -1,37 +1,43 @@
 /* eslint-disable array-callback-return */
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import apiBack from "../services/api";
-import '../css/dados.css'
+import "../css/dados.css";
 
-export default function ListarUsuarios (){
-    const [usuarios, setUsuarios] = useState([''])
+export default function ListarUsuarios() {
+  const [usuarios, setUsuarios] = useState([""]);
 
-    useEffect (()=>{
-        async function listarUsuario(){
-            const resposta = await apiBack.get('/ListarUsuarios')
-            setUsuarios(resposta.data)
-        }
-        listarUsuario()
-    },[usuarios])
-
-    async function excluirUsuario(id){
-        alert(id)
+  useEffect(() => {
+    async function listarUsuario() {
+      const resposta = await apiBack.get("/ListarUsuarios");
+      setUsuarios(resposta.data);
     }
+    listarUsuario();
+  }, []);
+  console.log(usuarios);
 
-    return (
-        <div className="id">
-            <h1>
-                Listar Dados De Usuario
-            </h1>
-            {usuarios.map((usuario)=>{
-                <div>
-                    <p key={usuario.id}>{usuario.id} - {usuario.nome}</p>
-                    <button Color='red' onClick={()=> excluirUsuario(usuario.id)}>p</button>
-                </div>
-            })}
-        </div>
-    )
+  async function excluirUsuario(id){
+      alert(id)
+      console.log(id)
+  }
 
+  return (
+    <div className="dados">
+      <h1>Listar Dados De Usuario</h1>
+      {usuarios.map((usuario) => {
 
+        return(
+
+        <article key={usuario.id}>
+          <p>
+            ID : {usuario.id}
+            <br/>
+            Nome: {usuario.nome}
+          <button Color='red' onClick={()=> excluirUsuario(usuario.id)}>p</button>
+          </p>
+        </article>
+        )
+      })}
+    </div>
+  );
 
 }
