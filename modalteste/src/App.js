b3f13b36424e7f3cb72c4ce395f2cdb93d6f28de
Peterson Geraldo <petersongeraldo94@gmail.com>
2024-03-06 13:47:00 -0300

    Att

--- modalteste/src/App.js
+++ modalteste/src/App.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
-import '../src/styles/styleapp.css'
+import { useEffect, useState } from "react";
+import "../src/styles/styleapp.css";
 import Modal from "react-modal";
+import apiLocal from "../src/api/apiLocal";
+
 export default function App() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [nome, setNome] = useState("");
+  const [clientes, setClientes] = useState([""]);
 
   function openModal() {
     setModalOpen(true);
   }
 
+  useEffect(() => {
+    async function mostarDados() {
+      await apiLocal.get("/ListarClientes");
+      setClientes(resposta.data)
+    }
+  }, []);
+
   function closeModal() {
     setModalOpen(false);
   }
 
-  function handleForms(){
-    alert('Pronto')
+  function handleForms() {
+    alert("Pronto");
   }
 
   return (
@@ -21,23 +32,18 @@ export default function App() {
       <h1>
         <p>Teste modal</p>
       </h1>
-      <input type='text'
-      placeholder="Digite seu nome"
-      />
-      <button onClick={handleForms} >Enviar</button>
-
+      <input type="text" placeholder="Digite seu nome" />
+      <button onClick={handleForms}>Enviar</button>
 
       <button onClick={openModal}>Abrir Modal</button>
-      <Modal isOpen={modalOpen} onRequestClose={closeModal}
-      
-      >
+      <Modal isOpen={modalOpen} onRequestClose={closeModal}>
         <h2>Modal Aberto</h2>
-        <form onSubmit={handleForms} >
+        <form onSubmit={handleForms}>
           <input type="text" placeholder="Email" />
           <input type="text" placeholder="Telefone" />
           <input type="text" placeholder="Endereço" />
           <input type="text" placeholder="Senha" />
-          <button type="submit" >Send</button>
+          <button type="submit">Send</button>
         </form>
         <button onClick={closeModal}>Close</button>
       </Modal>
